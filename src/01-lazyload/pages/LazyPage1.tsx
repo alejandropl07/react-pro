3b6c5542e630b6b542b9e4cb9b8580e644380e34
alejandropl07 <aprietoleon97@gmail.com>
2022-07-30 13:30:38 -0500

    Navigation modified

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,6 @@
+
+const LazyPage1 = () => {
+  return <h1>LazyPage1</h1>;
+};
+
+export default LazyPage1;
--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import LazyPage1 from "../01-lazyload/pages/LazyPage1";
+import LazyPage2 from "../01-lazyload/pages/LazyPage2";
+import LazyPage3 from "../01-lazyload/pages/LazyPage3";
 import logo from "../logo.svg";
 
 const Navigation = () => {
@@ -9,21 +12,21 @@ const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/">Home</NavLink>{" "}
+              <NavLink to="/lazy1">Lazy1</NavLink>{" "}
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>{" "}
+              <NavLink to="/lazy2">Lazy2</NavLink>{" "}
             </li>
             <li>
-              <NavLink to="/users">Users</NavLink>{" "}
+              <NavLink to="/lazy3">Lazy3</NavLink>{" "}
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/users" element={<h1>Users</h1>} />
+          <Route path="/lazy1" element={<LazyPage1 />} />
+          <Route path="/lazy2" element={<LazyPage2 />} />
+          <Route path="/lazy3" element={<LazyPage3 />} />
         </Routes>
       </div>
     </BrowserRouter>
